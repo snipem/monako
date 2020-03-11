18dc13db22f3812319fb79bdf02b9592a8330399
Matthias Küch <mail@matthias-kuech.de>
2020-03-11 14:23:39 +0100

    Asciidoc image fix

--- util_test.go
+++ util_test.go
@@ -1,6 +1,27 @@
 package main
 
+// run: make test
+
 import "testing"
+
+func TestAsciiDocImageFix(t *testing.T) {
+
+	noNeedToClean := "image:http://url/image.jpg[image,width=634,height=346]"
+	needToClean := "image:image2.png[image,width=634,height=346]"
+
+	clean := string(AsciidocPostprocessing([]byte(noNeedToClean)))
+
+	if clean != noNeedToClean {
+		t.Errorf("Clean was incorrect, got: %s, want: %s.", clean, noNeedToClean)
+	}
+
+	clean = string(AsciidocPostprocessing([]byte(needToClean)))
+	want := "image:../image2.png[image,width=634,height=346]"
+
+	if clean != want {
+		t.Errorf("Clean was incorrect, got: %s, want: %s.", clean, want)
+	}
+}
 
 func TestMarkdownFix(t *testing.T) {
 
