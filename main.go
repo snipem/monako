dc760bd6ca545518658cc755a75c9db85544345e
Matthias Küch <mail@matthias-kuech.de>
2020-03-21 20:36:29 +0100

    Add workaround for not displaying asciidoctor diagrams

--- main.go
+++ main.go
@@ -39,6 +39,8 @@ func main() {
 	}
 
 	cleanUp()
+	addFakeAsciidoctorBinForDiagramsToPath()
+
 	hugoRun([]string{"--quiet", "new", "site", "compose"})
 	getTheme(*hugoconfigfilepath, *menuconfigfilepath)
 
