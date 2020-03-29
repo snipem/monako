3a38d06cedee76af38c2693d1d4fe6b7494435da
Matthias Küch <mail@matthias-kuech.de>
2020-03-29 20:15:36 +0200

    Structure test

--- cmd/monako/main_test.go
+++ cmd/monako/main_test.go
@@ -3,6 +3,7 @@ package main
 // run: go test -v ./cmd/monako
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -30,22 +31,27 @@ func TestMain(t *testing.T) {
 		"-menu-config", "../../test/configs/only_markdown/config.menu.markdown.md"}
 	main()
 
-	assert.FileExists(t, filepath.Join(targetDir, "compose/config.toml"), "Hugo config is not present")
-	assert.FileExists(t, filepath.Join(targetDir, "compose/content/monako_menu_directory/index.md"), "Menu is not present")
-	assert.FileExists(t, filepath.Join(targetDir, "compose/public/docs/test/test_doc_markdown/index.html"), "Generated Test doc does not exist")
+	t.Run("Check for Hugo input files", func(t *testing.T) {
 
-	contentBytes, err := ioutil.ReadFile(filepath.Join(targetDir, "compose/public/docs/test/test_doc_markdown/index.html"))
-	content := string(contentBytes)
+		assert.FileExists(t, filepath.Join(targetDir, "compose/config.toml"), "Hugo config is not present")
+		assert.FileExists(t, filepath.Join(targetDir, "compose/content/monako_menu_directory/index.md"), "Menu is not present")
+	})
 
-	assert.NoError(t, err, "Can't read file")
+	t.Run("Check for generated test doc markdown page", func(t *testing.T) {
+		assert.FileExists(t, filepath.Join(targetDir, "compose/public/docs/test/test_doc_markdown/index.html"), "Generated Test doc does not exist")
 
-	assert.Contains(t, content, "<strong>Test docs</strong>", "Contains menu")
+		contentBytes, err := ioutil.ReadFile(filepath.Join(targetDir, "compose/public/docs/test/test_doc_markdown/index.html"))
+		content := string(contentBytes)
 
-	assert.Contains(t, content, "<img src=\"../profile.png\" alt=\"Picture in same folder\" />", "Contains relative picture")
-	assert.FileExists(t, filepath.Join(targetDir, "compose/public/docs/test/profile.png"), "Relative picture right placed")
+		assert.NoError(t, err, "Can't read file")
+		assert.Contains(t, content, "<strong>Test docs</strong>", "Contains menu")
 
-	assert.FileExists(t, filepath.Join(targetDir, "compose/public/docs/test/subfolder/subfolderprofile.png"), "Relative subfolder picture right placed")
-	assert.Contains(t, content, "<img src=\"../subfolder/subfolderprofile.png\" alt=\"Picture in sub folder\" />", "Contains relative picture")
+		assert.Contains(t, content, "<img src=\"../profile.png\" alt=\"Picture in same folder\" />", "Contains relative picture")
+		assert.FileExists(t, filepath.Join(targetDir, "compose/public/docs/test/profile.png"), "Relative picture right placed")
+
+		assert.FileExists(t, filepath.Join(targetDir, "compose/public/docs/test/subfolder/subfolderprofile.png"), "Relative subfolder picture right placed")
+		assert.Contains(t, content, "<img src=\"../subfolder/subfolderprofile.png\" alt=\"Picture in sub folder\" />", "Contains relative picture")
+	})
 
 	fs := http.FileServer(http.Dir(filepath.Join(targetDir, "compose/public/")))
 	ts := httptest.NewServer(http.StripPrefix("/", fs))
@@ -53,7 +59,7 @@ func TestMain(t *testing.T) {
 
 	t.Run("Check if images and sources are served", func(t *testing.T) {
 
-		content, err = getContent(ts, "")
+		content, err := getContent(ts, "/docs/test/test_doc_markdown/index.html")
 		assert.NoError(t, err, "HTTP Call failed")
 
 		urls, err := getURLKeyValuesFromHTML(content, "src", ts.URL)
@@ -61,22 +67,26 @@ func TestMain(t *testing.T) {
 			log.Fatal(err)
 		}
 
+		// TODO For some reason imgages seem to be ignored in urls
 		for _, url := range urls {
 			if strings.HasPrefix(url.String(), ts.URL) {
-				t.Logf("Checking for local served url %s", url.String())
+				// t.Logf("Checking for local served url %s", url.String())
 				// Check only if it's served, ignore content
 				_, err = getContent(ts, "")
 				assert.NoError(t, err, "URL is not served")
 			}
 		}
+
 	})
 
 	t.Run("Check contents of served page", func(t *testing.T) {
 
-		content, err = getContent(ts, "")
+		content, err := getContent(ts, "/docs/test/test_doc_markdown/index.html")
 		assert.NoError(t, err, "HTTP Call failed")
 
+		assert.Contains(t, content, "Ihr naht euch wieder, schwankende Gestalten!", "Does not contain Goethe")
 		assert.Contains(t, content, "Test docs", "Does not contain Menu header")
+		assert.Contains(t, content, "<h3 id=\"markdown-doc-3\">Markdown Doc 3</h3>", "Check rendered Markdown")
 
 	})
 
@@ -95,6 +105,26 @@ func getContent(ts *httptest.Server, url string) (string, error) {
 	defer res.Body.Close()
 	contentBytes, err := ioutil.ReadAll(res.Body)
 	return string(contentBytes), err
+}
+
+func TestURLKeyValues(t *testing.T) {
+
+	cases := []struct {
+		URL, Key, Base, Expected string
+	}{
+		{"<a href=\"/local/\"></a>", "href", "http://localhost", "http://localhost/local/"},
+		{"<img src=\"../image.png\"></a>", "src", "http://localhost/content/docs/", "http://localhost/content/image.png"},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("Extract %s from %s", tc.Key, tc.URL), func(t *testing.T) {
+			t.Parallel()
+			urls, err := getURLKeyValuesFromHTML(tc.URL, tc.Key, tc.Base)
+			assert.NoError(t, err, "Error at extraction")
+			assert.Equal(t, tc.Expected, urls[0].String())
+		})
+	}
+
 }
 
 func getURLKeyValuesFromHTML(content string, key string, baseURL string) ([]*url.URL, error) {
