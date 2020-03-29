8101b3cb3a235f271dbe0cb156380c8de36e504e
Matthias Küch <mail@matthias-kuech.de>
2020-03-29 20:43:30 +0200

    Fix logic

--- cmd/monako/main_test.go
+++ cmd/monako/main_test.go
@@ -91,8 +91,8 @@ func TestMain(t *testing.T) {
 
 	})
 
-	if !t.Failed() || runtime.GOOS == "windows" {
-		// Don't clean up when failed or on Windows (https://github.com/snipem/monako/pull/16/checks?check_run_id=543627521)
+	if !t.Failed() && runtime.GOOS != "windows" {
+		// Only clean up when not failed and not on Windows (https://github.com/snipem/monako/pull/16/checks?check_run_id=543627521)
 		filet.CleanUp(t)
 	}
 }
