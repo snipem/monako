f6e1c60c83945f5a3aabbffedc56e4f6e2780dcd
Matthias Küch <mail@matthias-kuech.de>
2020-03-31 17:37:40 +0200

    Add comments

--- pkg/helpers/util.go
+++ pkg/helpers/util.go
@@ -11,6 +11,7 @@ import (
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
+// FileIsWhitelisted returns true if the filename is in the whitelisted based on its suffix
 func FileIsWhitelisted(filename string, whitelist []string) bool {
 	for _, whitelisted := range whitelist {
 		if strings.HasSuffix(strings.ToLower(filename), strings.ToLower(whitelisted)) {
@@ -20,10 +21,12 @@ func FileIsWhitelisted(filename string, whitelist []string) bool {
 	return false
 }
 
+// IsMarkdown returns true if a file is a Markdown file
 func IsMarkdown(filename string) bool {
 	return strings.HasSuffix(strings.ToLower(filename), strings.ToLower(".md"))
 }
 
+// IsAsciidoc returns true if a file is a Asciidoc file
 func IsAsciidoc(filename string) bool {
 	return strings.HasSuffix(strings.ToLower(filename), strings.ToLower(".adoc")) ||
 		strings.HasSuffix(strings.ToLower(filename), strings.ToLower(".asciidoc")) ||
