62789e90596ff1511dd9cb96215ab7f045d5a32a
Matthias Küch <mail@matthias-kuech.de>
2020-04-04 23:33:22 +0200

    Structured command line options

--- cmd/monako/main.go
+++ cmd/monako/main.go
@@ -10,28 +10,55 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func main() {
+type CommandLineFlags struct {
+	ConfigFilePath     string
+	MenuConfigFilePath string
+	WorkingDir         string
+	BaseURL            string
+	Trace              bool
+	FailOnHugoError    bool
+}
+
+func parseCommandLine() (cliSettings CommandLineFlags) {
 
 	var configfilepath = flag.String("config", "config.monako.yaml", "Configuration file")
 	var menuconfigfilepath = flag.String("menu-config", "config.menu.md", "Menu file for monako-book theme")
 	var workingdir = flag.String("working-dir", ".", "Working dir for composed site")
-	var baseURLflag = flag.String("base-url", "", "Custom base URL")
+	var baseURL = flag.String("base-url", "", "Custom base URL")
 	var trace = flag.Bool("trace", false, "Enable trace logging")
 	var failOnHugoError = flag.Bool("fail-on-error", false, "Fail on document conversion errors")
 
 	flag.Parse()
 
-	if *trace {
+	return CommandLineFlags{
+		ConfigFilePath:     *configfilepath,
+		MenuConfigFilePath: *menuconfigfilepath,
+		WorkingDir:         *workingdir,
+		BaseURL:            *baseURL,
+		Trace:              *trace,
+		FailOnHugoError:    *failOnHugoError,
+	}
+}
+
+func main() {
+
+	cliSettings := parseCommandLine()
+
+	if cliSettings.Trace {
 		// Add line and filename to log
 		log.SetReportCaller(true)
 	}
 
-	config := compose.Init(*configfilepath, *menuconfigfilepath, *workingdir, *baseURLflag)
+	config := compose.Init(
+		cliSettings.ConfigFilePath,
+		cliSettings.MenuConfigFilePath,
+		cliSettings.WorkingDir,
+		cliSettings.BaseURL)
 
 	config.Compose()
 
 	err := config.Run()
-	if *failOnHugoError && err != nil {
+	if cliSettings.FailOnHugoError && err != nil {
 		log.Fatal(err)
 	}
 
