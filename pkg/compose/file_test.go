9e46c38a38482f147f1f7c5a7a33b48761f58179
Matthias Küch <mail@matthias-kuech.de>
2020-04-09 21:31:16 +0200

    Reordering

--- pkg/compose/file_test.go
+++ pkg/compose/file_test.go
@@ -3,56 +3,11 @@ package compose
 // run: go test  ./pkg/compose -run TestFrontmatterExpanding
 
 import (
-	"io/ioutil"
-	"log"
-	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestLoadConfig(t *testing.T) {
-	t.Run("Load good Config", func(t *testing.T) {
-		customWorkingdir := "workingdir123"
-		config, err := LoadConfig("../../test/config.local.yaml", customWorkingdir)
-		assert.NoError(t, err)
-
-		assert.Contains(t, config.ContentWorkingDir, customWorkingdir)
-		assert.NotEmpty(t, config.HugoWorkingDir, customWorkingdir)
-
-		assert.NotEmpty(t, config.Origins)
-
-	})
-
-	t.Run("Fail on missing config", func(t *testing.T) {
-		_, err := LoadConfig("missing path", "")
-		assert.Error(t, err)
-	})
-
-}
-
-func TestCleanUp(t *testing.T) {
-	config, _ := getTestConfig(t)
-
-	tmpFile := filepath.Join(
-		config.HugoWorkingDir,
-		"testfile.txt")
-
-	// Create the test data because it is not existing yet
-	err := os.Mkdir(config.HugoWorkingDir, standardFilemode)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	t.Logf("Using temp file %s", tmpFile)
-	_ = ioutil.WriteFile(tmpFile, []byte("none"), standardFilemode)
-
-	assert.FileExists(t, tmpFile, "File is existing that is to be cleaned up")
-	config.CleanUp()
-	assert.NoFileExists(t, tmpFile, "File seems not to be cleaned up, is stil present")
-
-}
 
 // TestLocalPath tests if the local file path calculation for remote files is correct
 func TestLocalPath(t *testing.T) {
--- pkg/compose/origin_test.go
+++ pkg/compose/origin_test.go
@@ -3,10 +3,56 @@ package compose
 // run: MONAKO_TEST_REPO="/tmp/testdata/monako-test" go test ./pkg/compose/
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestLoadConfig(t *testing.T) {
+	t.Run("Load good Config", func(t *testing.T) {
+		customWorkingdir := "workingdir123"
+		config, err := LoadConfig("../../test/config.local.yaml", customWorkingdir)
+		assert.NoError(t, err)
+
+		assert.Contains(t, config.ContentWorkingDir, customWorkingdir)
+		assert.NotEmpty(t, config.HugoWorkingDir, customWorkingdir)
+
+		assert.NotEmpty(t, config.Origins)
+
+	})
+
+	t.Run("Fail on missing config", func(t *testing.T) {
+		_, err := LoadConfig("missing path", "")
+		assert.Error(t, err)
+	})
+
+}
+
+func TestCleanUp(t *testing.T) {
+	config, _ := getTestConfig(t)
+
+	tmpFile := filepath.Join(
+		config.HugoWorkingDir,
+		"testfile.txt")
+
+	// Create the test data because it is not existing yet
+	err := os.Mkdir(config.HugoWorkingDir, standardFilemode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t.Logf("Using temp file %s", tmpFile)
+	_ = ioutil.WriteFile(tmpFile, []byte("none"), standardFilemode)
+
+	assert.FileExists(t, tmpFile, "File is existing that is to be cleaned up")
+	config.CleanUp()
+	assert.NoFileExists(t, tmpFile, "File seems not to be cleaned up, is stil present")
+
+}
 
 func TestGitCommiter(t *testing.T) {
 
